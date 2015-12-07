e120f872db0dc662ab9b8ae1ccfa52e59f7211a3
Andrew Burnie <apburnie@hotmail.co.uk>
2015-12-07 17:35:42 +0000

    Stage 8 with cat completed

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -3,3 +3,18 @@ require 'sinatra'
 get '/' do
   "Hello World!"
 end
+
+get '/secret' do
+  "sshh"
+end
+
+get '/shotgun_test' do
+  "Working"
+end
+
+get '/cat' do
+  # Alternatively could put html in separate file: File.read('cat_html_file.html')
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
